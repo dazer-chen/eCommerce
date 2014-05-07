fa095a1a18c48a9258c7d523edade1f6d908aa6a
Matheus Eduardo Machado Moreira <matheus.emm@gmail.com>
2014-05-06 21:48:57 -0300

    fixed warning in browser.js related to http://facebook.github.io/react/docs/multiple-components.html#dynamic-children - each <Product> should have a 'key' property

--- public/javascripts/browser.js
+++ public/javascripts/browser.js
@@ -4,7 +4,6 @@ var Product = React.createClass({
 	render: function() {
 		return (
 			<tr>
-				<td><span className="hidden">{this.props.data.product.id}</span></td>
 				<td>{this.props.data.product.name}</td>
 				<td>{this.props.data.product.description}</td>
 				<td>{this.props.data.product.price}</td>
@@ -18,7 +17,7 @@ var Browser = React.createClass({
 	__loadProducts: function() {
 		$.ajax({
 			url: this.props.url,
-			dataType: 'json',
+			dataType: "json",
 			success: function(data) {
 				this.setState({ products: data });
 			}.bind(this)
@@ -32,24 +31,26 @@ var Browser = React.createClass({
 		// setInterval(this.__loadProducts(), this.props.pollInterval);
 	},
 	render: function() {
-		var productNodes = this.state.products.map(function (prod) {
-			return <Product data={prod} />;
+		var productNodes = this.state.products.map(function (p) {
+			return <Product data={p} key={p.product.id} />;
 		});
+		
 		return (
-			<table className="browser table table-striped">
-				<thead>
-					<tr>
-						<th></th>
-						<th>Name</th>
-						<th>Description</th>
-						<th>Price</th>
-						<th>Quantity</th>
-					</tr>
-				</thead>
-				<tbody>
-					{productNodes}
-				</tbody>
-			</table>
+			<div className="browser">
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Description</th>
+							<th>Price</th>
+							<th>Quantity</th>
+						</tr>
+					</thead>
+						<tbody>
+							{productNodes}
+						</tbody>
+				</table>
+			</div>
 		);
 	}
 });
