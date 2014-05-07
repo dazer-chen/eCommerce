10a9ad08a45418b262477ecddceb4b50bff07cd9
Matheus Moreira <matheus.moreira@sigga.com.br>
2014-05-07 12:23:18 -0300

    buttons tooltips; shopping cart basic functions at index - add to cart and cart items count update

--- public/javascripts/browser.js
+++ public/javascripts/browser.js
@@ -1,15 +1,17 @@
 /** @jsx React.DOM */
 
 var ShoppingCartButton = React.createClass({
-	getInitialState: function() {
-		return ({ count: 0 });
-	},
 	render: function() {
 		var cartUrl = jsRoutes.controllers.Application.shoppingCart().url;
 		return (
 			<div className="shoppingCartBtn text-right">
-				<a className="btn btn-default btn-sm" href={cartUrl}>
-					<span className="glyphicon glyphicon-shopping-cart"></span> Shopping Cart ({this.state.count})
+				<a
+					href={cartUrl}
+					className="btn btn-default"
+					title="Shopping cart"
+					data-toggle="tooltip">
+
+					<span className="glyphicon glyphicon-shopping-cart"></span> {this.props.count}
 				</a>
 			</div>
 		);
@@ -24,6 +26,16 @@ var Product = React.createClass({
 				<td>{this.props.data.product.description}</td>
 				<td>{this.props.data.product.price}</td>
 				<td>{this.props.data.quantity}</td>
+				<td>
+					<button 
+						className="addToCart btn btn-default btn-sm"
+						title="Add to cart"
+						onClick={this.props.addToCartHandler.bind(null, this.props.key)}
+						data-toggle="tooltip">
+
+						<span className="glyphicon glyphicon-plus"></span>
+					</button>
+				</td>
 			</tr>
 		);
 	}
@@ -39,21 +51,26 @@ var Browser = React.createClass({
 			}.bind(this)
 		});
 	},
+	__addToCart: function(productId) {
+		console.log("adding product #" + productId + " to cart");
+		this.setState({ cartCount: this.state.cartCount + 1 });
+	},
 	getInitialState: function() {
-		return { products: [] };
+		return { products: [], cartCount: 0 };
 	},
 	componentWillMount: function() {
 		this.__loadProducts();
 		// setInterval(this.__loadProducts(), this.props.pollInterval);
 	},
 	render: function() {
+		var handler = this.__addToCart
 		var productNodes = this.state.products.map(function (p) {
-			return <Product data={p} key={p.product.id} />;
+			return <Product data={p} key={p.product.id} addToCartHandler={handler} />;
 		});
 		
 		return (
 			<div className="browser">
-				<ShoppingCartButton />
+				<ShoppingCartButton count={this.state.cartCount} />
 				
 				<table className="table table-striped">
 					<thead>
@@ -62,6 +79,7 @@ var Browser = React.createClass({
 							<th>Description</th>
 							<th>Price</th>
 							<th>Quantity</th>
+							<th></th>
 						</tr>
 					</thead>
 						<tbody>
